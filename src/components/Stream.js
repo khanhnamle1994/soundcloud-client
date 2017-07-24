2b2434649fe545492a3e6df9dc16c8e6da7b9e61
James Le <le_j6@denison.edu>
2017-07-23 22:18:28 -0700

    Refactor ES6 Class Component to a Stateless Functional Component

--- src/components/Stream.js
+++ src/components/Stream.js
@@ -1,33 +1,15 @@
 import React from 'react';
 
-class Stream extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {};
-  }
-
-  render() {
-    const { tracks = [] } = this.props;
-
-    return (
-      <div>
-        {
-          tracks.map((track, key) => {
-            return (
-              <div className="track" key={key}>
-                {track.title}
-                <button onClick={() => this.setState({ [key]: !this.state[key] })} type="button">
-                  { this.state[key] ? 'Dislike' : 'Like'}
-                </button>
-              </div>
-            );
-          })
-        }
-      </div>
-    );
-  }
-
+function Stream({ tracks = [] }) {
+  return (
+    <div>
+      {
+        tracks.map((track, key) => {
+          return <div className="track" key={key}>{track.title}</div>;
+        })
+      }
+    </div>
+  );
 }
 
 export default Stream;
