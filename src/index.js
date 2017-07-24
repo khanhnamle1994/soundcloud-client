bd1299189084041316b52901636c58c2f733059c
James Le <le_j6@denison.edu>
2017-07-24 16:01:17 -0700

    Dispatching an Action

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
 import Stream from './components/Stream';
 
 const tracks = [
@@ -11,8 +13,11 @@ const tracks = [
   }
 ];
 
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
+
 ReactDOM.render(
-  <Stream tracks={tracks} />,
+  <Stream />,
   document.getElementById('app')
 );
 
