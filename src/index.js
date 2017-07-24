266e2320230a8d3b7524315c7a25aff6b5c3be28
James Le <le_j6@denison.edu>
2017-07-24 16:18:22 -0700

    Connect Redux and React

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
 import * as actions from './actions';
 import Stream from './components/Stream';
@@ -17,7 +18,9 @@ const store = configureStore();
 store.dispatch(actions.setTracks(tracks));
 
 ReactDOM.render(
-  <Stream />,
+  <Provider store={store}>
+    <Stream />
+  </Provider>,
   document.getElementById('app')
 );
 
