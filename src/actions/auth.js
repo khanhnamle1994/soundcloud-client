f58cea10fbcd70dc48955fdb90204698f6c35952
James Le <le_j6@denison.edu>
2017-07-25 01:40:30 -0700

    Set Me

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,15 +1,27 @@
 import SC from 'soundcloud';
+import * as actionTypes from '../constants/actionTypes';
+
+function setMe(user) {
+  return {
+    type: actionTypes.ME_SET,
+    user
+  };
+}
 
 export function auth() {
-  SC.connect().then((session) => {
-    fetchMe(session);
-  });
+  return function (dispatch) {
+    SC.connect().then((session) => {
+      fetchMe(session);
+    });
+  };
 };
 
 function fetchMe(session) {
-  fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
+  return function (dispatch) {
+    fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
+      .then((response) => response.json())
+      .then((data) => {
+        dispatch(setMe(data));
     });
+  };
 }
--- src/components/Stream/index.js
+++ src/components/Stream/index.js
@@ -5,8 +5,10 @@ import * as actions from '../../actions';
 import Stream from './presenter';
 
 function mapStateToProps(state) {
+  const { user } = state.auth;
   const tracks = state.track;
   return {
+    user,
     tracks
   }
 }
