99907259a9a4b8803abeaeaa05a52a29bc11a9d9
James Le <le_j6@denison.edu>
2017-07-25 01:21:16 -0700

    React Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import SC from 'soundcloud';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
 import configureStore from './stores/configureStore';
+import App from './components/App';
+import Callback from './components/Callback';
 import * as actions from './actions';
 import Stream from './components/Stream';
 import { CLIENT_ID, REDIRECT_URI } from './constants/auth';
@@ -21,9 +25,17 @@ const tracks = [
 const store = configureStore();
 store.dispatch(actions.setTracks(tracks));
 
+const history = syncHistoryWithStore(browserHistory, store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <Stream />
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Stream} />
+        <Route path="/" component={Stream} />
+        <Route path="/callback" component={Callback} />
+      </Route>
+    </Router>
   </Provider>,
   document.getElementById('app')
 );
