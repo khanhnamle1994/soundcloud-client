608095e904836deeaf2919e88d82ee6cbc02b3ee
James Le <le_j6@denison.edu>
2017-07-30 12:43:54 -0700

    MobX Actions

--- mobx-extension/src/actions/auth.js
+++ mobx-extension/src/actions/auth.js
@@ -1,39 +1,26 @@
 import SC from 'soundcloud';
-import * as actionTypes from '../constants/actionTypes';
-import { setTracks } from '../actions/track';
-
-function setMe(user) {
-  return {
-    type: actionTypes.ME_SET,
-    user
-  };
-}
+import userStore from '../stores/userStore';
+import trackStore from '../stores/trackStore';
 
 export function auth() {
-  return function (dispatch) {
-    SC.connect().then((session) => {
-      dispatch(fetchMe(session));
-      dispatch(fetchStream(session));
-    });
-  };
+  SC.connect().then((session) => {
+    fetchMe(session);
+    fetchStream(session);
+  });
 };
 
 function fetchMe(session) {
-  return function (dispatch) {
-    fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch(setMe(data));
+  fetch(`//api.soundcloud.com/me?oauth_token=${session.oauth_token}`)
+    .then((response) => response.json())
+    .then((me) => {
+      userStore.me = me;
     });
-  };
 }
 
 function fetchStream(session) {
-  return function (dispatch) {
-    fetch(`//api.soundcloud.com/me/activities?limit=20&offset=0&oauth_token=${session.oauth_token}`)
-      .then((response) => response.json())
-      .then((data) => {
-        dispatch(setTracks(data.collection));
-      });
-  };
+  fetch(`//api.soundcloud.com/me/activities?limit=20&offset=0&oauth_token=${session.oauth_token}`)
+    .then((response) => response.json())
+    .then((data) => {
+      trackStore.tracks = data.collection;
+    });
 }
