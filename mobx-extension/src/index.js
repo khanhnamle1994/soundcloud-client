106aab49aa219a907f1f77d70c10011550a22a41
James Le <le_j6@denison.edu>
2017-07-30 13:08:34 -0700

    Provide and Inject

--- mobx-extension/src/index.js
+++ mobx-extension/src/index.js
@@ -1,24 +1,31 @@
 import SC from 'soundcloud';
+import { Provider } from 'mobx-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { useStrict } from 'mobx';
 import App from './components/App';
 import Callback from './components/Callback';
-import Stream from './components/Stream';
+import StreamContainer from './components/Stream';
+import userStore from './stores/userStore';
+import trackStore from './stores/trackStore';
 import { CLIENT_ID, REDIRECT_URI } from './constants/auth';
 
 SC.initialize({ client_id: CLIENT_ID, redirect_uri: REDIRECT_URI });
 
 useStrict(true);
 
+const stores = { userStore, trackStore };
+
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Stream} />
-      <Route path="/" component={Stream} />
-      <Route path="/callback" component={Callback} />
-    </Route>
-  </Router>,
+  <Provider { ...stores }>
+    <Router history={browserHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Stream} />
+        <Route path="/" component={Stream} />
+        <Route path="/callback" component={Callback} />
+      </Route>
+    </Router>
+  </Provider>,
   document.getElementById('app')
 );
