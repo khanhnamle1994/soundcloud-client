458c99bc8c7d8aa1882b4fc5177fa3329657daeb
James Le <le_j6@denison.edu>
2017-07-30 00:03:28 -0700

    Epics Middleware in Redux

--- observable-extension/src/actions/index.js
+++ observable-extension/src/actions/index.js
@@ -1,8 +1,16 @@
-import { auth } from './auth';
+import { combineEpics } from 'redux-observable';
+import { auth, authEpic, fetchMeEpic, fetchStreamEpic } from './auth';
 import { setTracks, playTrack } from './track';
+
+const rootEpic = combineEpics(
+  authEpic,
+  fetchMeEpic,
+  fetchStreamEpic
+);
 
 export {
   auth,
   setTracks,
-  playTrack
+  playTrack,
+  rootEpic
 };
